7868a6faa1ed6c69ecf86613843ed9e7a8bccac5
Josh Arias <josh.d.arias@gmail.com>
2018-09-20 19:59:07 -0700

    Took more notes on Week 1

--- Coursera/DataScienceJohnHopkins/02-R_Programming/Week1Notes.R
+++ Coursera/DataScienceJohnHopkins/02-R_Programming/Week1Notes.R
@@ -1,22 +1,40 @@
-## Background material
+# Background material
 
-# Check working directory.
+## Check working directory.
 getwd()
 dir()
 
-# Change working directory
+## Change working directory
 setwd("C:/Users/joshd/Desktop/grunt-work/Coursera/DataScienceJohnHopkins/02-R_Programming/")
 
-# Reference function
+## Reference function
 myfunction()
 
-# Make both functions available without running it first.
+## Make both functions available without running it first.
 source("MyCode.R")
 
-# Run second code in MyCode.R only.
+## Run second code in MyCode.R only.
 second(4)
 second(4:10)
 
-# Everytime you change your code, you must use the
-# source() function to load your code functions into
-# R again.
+### Everytime you change your code, you must use the
+### source() function to load your code functions into
+### R again.
+
+# Free Software
+
+## With free software, you have the freedom to:
+### Run the program for any purpose
+### Study and adapt the program as you see fit.
+### Redistribute copies of the program.
+### Improve and release the program.
+
+# Drawbacks of R
+
+## No helpline; if the feature is not there, you must make it
+## Limited to physical memory of machine you are on.
+
+# Design of the R system
+## The "base" R system has the base packages needed
+## ...and everything else. Over 4000+ packages
+
