b41e67a1fd93f961483468daca0d8b2a7b1df8c6
Joshua Arias <josh.d.arias@gmail.com>
2018-10-25 08:50:20 -0700

    10/25/18 Swirl Notes

--- Coursera/DataScienceJohnHopkins/02-R_Programming/SwirlNotes.R
+++ Coursera/DataScienceJohnHopkins/02-R_Programming/SwirlNotes.R
@@ -14,13 +14,9 @@ install_from_swirl("R Programming")
 ## Swirl Lesson 2: Workspace and Files
 # file.exists() great for when you need to run a program that loops through a series of files and does processing
 # on each one, because you need to check if a file exists before you process it
-
 # file.info() allows you access info about a certain file in your WD.
-
 # file.rename(from,to)
-
 # file.copy(from,to)
-
 # file.path(file), gives the path to a file
 
 ## Swirl Lesson 4: Vectors
@@ -31,34 +27,33 @@ install_from_swirl("R Programming")
 
 ## Swirl Lesson 6: Subsetting Vectors
 # Three types of index vectors: logical, positive, negative, and named.
-
 # Not only can you call a subset vector with x[1:10], but you can also make a conditional statement, such as:
 # x[!is.na(x) & x>0] which will give a subset vector of positive numbers that are not NA in x.
-
 # It can also be a combine vector: x[c(1,3,5)] for the 1st, 3rd, and 5th numbers in vector x.
-
 # To exclude specific point in the index, we can use negative indices, like so:
 # x[-c(2,10)] which prints x except the 2nd and 10th index numbers
-
 # To "name" indices, we can do this:
 # x <- c(foo=11,bar=2,norf=NA)
-
 # Name indices after the fact with the names() function
-
 # Check for identity using indentical() function
 
 ## Swirl Lesson 7: Matrices and Data Frames
 # Setting a "vector's" dimension:
 # dim(x) <- c(5,4) to give "x" 4 rows and 5 columns. So we've made it a matrix!
-
 # Combining matrices columnally:
 # cbind(matrix1, matrix2), but remember that matrices can only contain ONE class of data (e.g., strings)
 # data.frame allows us to COMBINE DIFFERENT CLASSES OF DATA intoone matrix
-
 # Specifying column names:
 # colnames(data frame) <- string vector
 # rownames() for rows
 
+## Swirl Lesson 8: Logic
+# && and || only look at the first indexed value in a vector in an expression
+# which() lets you know which indices in a LOGICAL vector contain TRUEs.
+# any() returns TRUE if one or more of the elements in the LOGICAL vector is TRUE
+# all() will return TRUE if EVERY element in the LOGICAL vector is TRUE
 
+## Swirl Lesson 9: Functions
+# Sys.Date() returns a Date class object (today's actual date)
 
 
