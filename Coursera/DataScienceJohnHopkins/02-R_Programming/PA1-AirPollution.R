07c7837abe69ff1eee4b5a2b3574671b2bf37e45
Joshua Arias <josh.d.arias@gmail.com>
2018-10-27 18:32:58 -0700

    Programming Assignment 1 Cont

--- Coursera/DataScienceJohnHopkins/02-R_Programming/PA1-AirPollution.R
+++ Coursera/DataScienceJohnHopkins/02-R_Programming/PA1-AirPollution.R
@@ -31,6 +31,30 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
 }
 
 complete <- function(directory, id = 1:332) {
-  
+  setwd("~/Desktop/grunt-work/Coursera/DataScienceJohnHopkins/02-R_Programming/")
+  nobs <- 0
+  for (i in id) {
+    if(i < 10) {
+      j <- paste0("00",i)
+    } else if(10 <= i & i < 100) {
+      j <- paste0("0",i)
+    } else {
+      j <- as.character(i)
+    }
+    ndir <- paste0(directory,"/",j,".csv")
+    data <- read.csv(ndir)
+    attach(data)
+    s <- sulfate
+    n <- nitrate
+    detach(data)
+    for (j in 1:length(s))
+      if(is.na(s[j]) == FALSE && is.na(n[j]) == FALSE) {
+        nobs <- nobs + 1
+      } else {
+        next
+      }
+  }
+  print(nobs)
+  # Now append to a new data array
 }
   
