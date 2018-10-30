d87b77db7d0d2f4c1e2a39c470f215b743581801
Joshua Arias <josh.d.arias@gmail.com>
2018-10-30 10:49:48 -0700

    Testing

--- Coursera/DataScienceJohnHopkins/02-R_Programming/PA1-AirPollution.R
+++ Coursera/DataScienceJohnHopkins/02-R_Programming/PA1-AirPollution.R
@@ -33,7 +33,10 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
 complete <- function(directory, id = 1:332) {
   setwd("~/Desktop/grunt-work/Coursera/DataScienceJohnHopkins/02-R_Programming/")
   nobs <- 0
+  mat <- matrix(ncol=2,nrow=1)
+  colnames(mat) <- c("id","nobs")
   for (i in id) {
+    nobs <- 0
     if(i < 10) {
       j <- paste0("00",i)
     } else if(10 <= i & i < 100) {
@@ -47,14 +50,56 @@ complete <- function(directory, id = 1:332) {
     s <- sulfate
     n <- nitrate
     detach(data)
-    for (j in 1:length(s))
+    for (j in 1:length(s)) {
       if(is.na(s[j]) == FALSE && is.na(n[j]) == FALSE) {
         nobs <- nobs + 1
       } else {
         next
       }
+    }
+    newrow <- c(i,nobs)
+    mat <- rbind(mat,newrow)
   }
-  print(nobs)
-  # Now append to a new data array
+  rownames(mat) <- c()
+  mat <- mat[-c(1),]
+  mat <- as.data.frame(mat)
+  print(mat)
 }
-  
+
+corr <- function(directory, threshold = 0) {
+  setwd("~/Desktop/grunt-work/Coursera/DataScienceJohnHopkins/02-R_Programming/")
+  nobs <- 0
+  id <- 1:332
+  mat <- matrix(ncol=1,nrow=1)
+  for (i in id) {
+    nobs <- 0
+    if(i < 10) {
+      j <- paste0("00",i)
+    } else if(10 <= i & i < 100) {
+      j <- paste0("0",i)
+    } else {
+      j <- as.character(i)
+    }
+    ndir <- paste0(directory,"/",j,".csv")
+    data <- read.csv(ndir)
+    attach(data)
+    s <- sulfate
+    n <- nitrate
+    detach(data)
+    for (j in 1:length(s)) {
+      if(is.na(s[j]) == FALSE && is.na(n[j]) == FALSE) {
+        nobs <- nobs + 1
+      } else {
+        next
+      }
+    }
+    if(nobs > threshold) {
+      corre <- cor(s,n,use="complete.obs")
+      mat <- rbind(mat,corre)
+    } else {
+      next
+    }
+  }
+  rownames(mat) <- c()
+  mat[-c(1)]
+}
